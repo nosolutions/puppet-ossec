8e269967a1918abf3d5cdc7220b36a1d2660a7a7
Toni Schmidbauer <toni@stderr.at>
2014-10-24 13:51:49 +0200

    added test when enable_atomic is false

--- spec/classes/ossec_spec.rb
+++ spec/classes/ossec_spec.rb
@@ -1,18 +1,41 @@
 require 'spec_helper'
 
 describe 'ossec' do
-  let(:node) { 'foo.example.com' } 
-  let :facts do
-    {
-      :osfamily       => 'RedHat',
-      :ipaddress_eth0 => '192.168.1.1',
-      :network_eth0   => '192.168.1.0',
-      :netmask_eth0   => '255.255.255.0',
-    }
+
+  context 'when running with default parameters' do
+    let(:node) { 'foo.example.com' }
+    let :facts do
+      {
+        :osfamily       => 'RedHat',
+        :ipaddress_eth0 => '192.168.1.1',
+        :network_eth0   => '192.168.1.0',
+        :netmask_eth0   => '255.255.255.0',
+      }
+    end
+
+
+    it { should create_class('ossec') }
+    it { should contain_class('ossec::params') }
+
+    it { should contain_class('atomic') }
   end
 
-  it { should create_class('ossec') }
-  it { should contain_class('ossec::params') }
+  context 'when enable_atomic is false on RedHat' do
+    let(:node) { 'foo.example.com' }
+    let :facts do
+      {
+        :osfamily       => 'RedHat',
+        :ipaddress_eth0 => '192.168.1.1',
+        :network_eth0   => '192.168.1.0',
+        :netmask_eth0   => '255.255.255.0',
+      }
+    end
+    let :params do
+      {
+        'enable_atomic' => false,
+      }
+    end
 
-  it { should contain_class('atomic') }
+    it { should_not contain_class('atomic') }
+  end
 end
